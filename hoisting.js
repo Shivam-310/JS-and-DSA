a73d9c979906b5d9235d5f0a9ec4670d3ba25112
Shivam <shivamchaudhary310899@gmail.com>
2024-09-17 22:54:10 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+function myFunc() {
+    console.log("Before declaration:", myVar); // Output: undefined
+    var myVar = "Hello, world!";
+    console.log("After declaration:", myVar); // Output: "Hello, world!"
+  }
+  
+  myFunc();
+  
