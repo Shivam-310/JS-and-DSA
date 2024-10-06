1d8d16996cbd07ebc7c1063e7f66f2dd707cddbb
Shivam <shivamchaudhary310899@gmail.com>
2024-10-06 21:37:57 +0530

    password generator

---
+++ randomPassword.js
@@ -0,0 +1,10 @@
+function randomPassword(length = 10) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const index = Math.floor(Math.random() * chars.length);
+      password += chars[index];
+    }
+    return password;
+  }
+  
