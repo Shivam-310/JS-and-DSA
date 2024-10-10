bfd0fc300d7e7a4156e09c7fb70e5c36a599cc99
Shivam <shivamchaudhary310899@gmail.com>
2024-10-10 22:01:38 +0530

    initial

---
+++ SumAndLargets.js
@@ -0,0 +1,15 @@
+let number1 = parseFloat(prompt("enter number"));
+let number2 = parseFloat(prompt("enter number"));
+
+let sum = num1 + num2;
+console.log("the sum is" + sum);
+
+
+
+
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let num3 = parseFloat(prompt("Enter the third number:"));
+
+let max = Math.max(num1, num2, num3);
+console.log("The largest number is: " + max);
